c6b297836674bef27d2797e736980b0aa9cd0165
mateus <martinsmateus382@gmail.com>
2023-06-12 11:43:45 -0300

    feature de confirmação envio de email

--- src/pages/contate-nos.jsx
+++ src/pages/contate-nos.jsx
@@ -1,38 +1,34 @@
-import React, { useEffect, useState ,useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { useSpring, animated } from 'react-spring';
+import checkIcon from '../../public/assets/images/check_icon.png'
 import axios from "axios";
 
 const ContactForm = () => {
-
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [description, setDescription] = useState("");
   const [showDiv, setShowDiv] = useState(false);
-  const form = document.querySelector('#form_contact_us');
-  const [confirmation, Setconfirmation] = useState(false);
-  function limparFormulario() {
-    form.reset();
-  }
+  const [confirmation, setConfirmation] = useState(false);
 
   const slideAnimation = useSpring({
     to: {
       opacity: showDiv ? 1 : 0,
-    transform: showDiv ? 'translateX(0)' : 'translateX(100%)',
+      transform: showDiv ? 'translateX(0)' : 'translateX(100%)',
     },
     config: {
       duration: 500
     }
   });
 
-  useEffect(()=> {
-    setShowDiv(true)
-  })
+  useEffect(() => {
+    setShowDiv(true);
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-  
+
     const requestData = {
       name,
       email,
@@ -43,21 +39,30 @@ const ContactForm = () => {
     try {
       // Envie o e-mail
       await axios.post('https://back-end-teusmamf.vercel.app/send_email', requestData);
-      Setconfirmation(true)
+      setConfirmation(true);
       console.log('E-mail enviado com sucesso!');
 
+      // Resetar a confirmação após 3 segundos
+      setTimeout(() => {
+        setConfirmation(false);
+      }, 3000);
     } catch (error) {
-
       console.error('Erro ao enviar o e-mail:', error);
     }
-    
+
     limparFormulario();
-    
+  };
+
+  const limparFormulario = () => {
+    setName("");
+    setEmail("");
+    setSubject("");
+    setDescription("");
   };
 
   return (
     <div className="contact-form">
-      <form onSubmit={handleSubmit} id="form_contact_us" >
+      <form onSubmit={handleSubmit} id="form_contact_us">
         <label htmlFor="name">Nome:</label>
         <input
           type="text"
@@ -66,7 +71,7 @@ const ContactForm = () => {
           onChange={(e) => setName(e.target.value)}
           required
         />
-        <br></br>
+        <br />
         <label htmlFor="email">Email:</label>
         <input
           className="input_email"
@@ -76,7 +81,7 @@ const ContactForm = () => {
           onChange={(e) => setEmail(e.target.value)}
           required
         />
-        <br></br>
+        <br />
         <label htmlFor="subject">Assunto:</label>
         <select
           id="subject"
@@ -91,7 +96,7 @@ const ContactForm = () => {
           <option value="Selos">Selos</option>
           <option value="Assunto 3">Outros</option>
         </select>
-        <br></br>
+        <br />
         <label htmlFor="description" className="description">Descrição:</label>
         <textarea
           value={description}
@@ -99,21 +104,36 @@ const ContactForm = () => {
           required
         ></textarea>
 
-        <button type="submit">Enviar</button> <span className={confirmation ? "confirmation_email" : "confirmation_email_visible" }>Email enviado!</span>
-        
+        <button type="submit">Enviar</button>
+        {confirmation && (
+          <div><span className="confirmation_email">Email enviado!</span> 
+            <img src={checkIcon} alt="check_icon" className="check_icon"/>
+          </div>
+        )}
       </form>
 
       <animated.div className="let_get_in_touch" style={slideAnimation}>
         <h3>Entre em contato</h3>
-
-        <svg fill="#5c0101" height="800px" width="800px" className="img_contact_us" version="1.1" id="Filled_Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
-	          y="0px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
-              <g id="Contact-Us-Filled">
-	          <path d="M1,1v17h4v4l8.5-4H22V1H1z M8,11H5V8h3V11z M13,11h-3V8h3V11z M18,11h-3V8h3V11z"/>
-              </g>
+        <svg
+          fill="#5c0101"
+          height="800px"
+          width="800px"
+          className="img_contact_us"
+          version="1.1"
+          id="Filled_Icons"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          x="0px"
+          y="0px"
+          viewBox="0 0 24 24"
+          enableBackground="new 0 0 24 24"
+          xmlSpace="preserve"
+        >
+          <g id="Contact-Us-Filled">
+            <path d="M1,1v17h4v4l8.5-4H22V1H1z M8,11H5V8h3V11z M13,11h-3V8h3V11z M18,11h-3V8h3V11z" />
+          </g>
         </svg>
       </animated.div>
-     
     </div>
   );
 };
